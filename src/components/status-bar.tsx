f81fbbefdd8a2d223a0408e11d90da45505b64af
Nitsan Amit <Nitsan135@gmail.com>
2020-11-11 18:43:14 +0200

    add important word

--- src/components/status-bar.tsx
+++ src/components/status-bar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const StatusBar: React.FC<StatusBarProps> = ({score}) => {
     return (
         <div>
-            <h3>{`Score: ${score}`}</h3>
+            <h3>{`User score: ${score}`}</h3>
             {/*<NextHexagon /> */}
             {/*TODO Assaf you do this ok?*/}
         </div>
